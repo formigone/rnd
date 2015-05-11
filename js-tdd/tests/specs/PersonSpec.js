c224e30a4c830936ee677acd72d7d48c25de7c00
Rodrigo Silveira <rsilveira@deseretdigital.com>
2015-05-11 18:40:39 -0300

    more tests

--- js-tdd/tests/specs/PersonSpec.js
+++ js-tdd/tests/specs/PersonSpec.js
@@ -3,5 +3,11 @@ describe('Person class', function(){
       var me = new Person('rodrigo', 'silveira');
       expect(me.getName()).toBe('rodrigo silveira');
    });
+
+   it('should have first name only', function(){
+      var me = new Person('rodrigo');
+      expect(me.firstName.length).toBe(7);
+      expect(me.lastName).toBe(undefined);
+   });
 });
 
